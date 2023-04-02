de8ef322a997e074b6c81533e33b416fa3147bc2
Jonathan Romain <romainjonathan43@gmail.com>
2023-04-02 13:16:18 -0500

    Codewars Challenge 4-2-2023

---
+++ 8kyu/4-2-2023/Function 1 = hello world/main.js
@@ -0,0 +1,42 @@
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+
+
+// Write a function "greet" that returns "hello world!"
+// function greet(){
+//   return `hello world!`
+// }
+
+const greet = () => `hello world!`
+
+
+
+// Write a function "greet" that returns "hello world!"
+function greet () {
+    const world = "hello";
+    let hello = "world";
+    let stop = "!"
+    return world +" "+ hello + stop;
+  }
+
+  // Write a function "greet" that returns "hello world!"
+function greet () {
+    const world = "hello";
+    let hello = "world";
+    let stop = "!"
+    return world +" "+ hello + stop;
+  }
+
+  // Write a function "greet" that returns "hello world!"
+function greet(){
+    return "hello world!"
+  }
+
+  // Write a function "greet" that returns "hello world!"
+function greet(){
+    let result = "hello world!"
+    return result;
+  }
