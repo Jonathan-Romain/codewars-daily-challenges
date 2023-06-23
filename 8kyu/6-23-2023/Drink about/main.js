557c2dc9b60c1572bcfedcc31d78f8383565b511
Jonathan Romain <romainjonathan43@gmail.com>
2023-06-23 10:46:15 -0500

    Codewars Challenge 6-23-2023

---
+++ 8kyu/6-23-2023/Drink about/main.js
@@ -0,0 +1,43 @@
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+// Examples: (Input --> Output)
+
+// 13 --> "drink toddy"
+// 17 --> "drink coke"
+// 18 --> "drink beer"
+// 20 --> "drink beer"
+// 30 --> "drink whisky"
+
+
+
+function peopleWithAgeDrink(old) {
+    if(old < 14){
+      return 'drink toddy';
+    } else if(old <= 17 && old >= 14){
+      return 'drink coke';
+    } else if(old >= 18 && old <= 20){
+      return 'drink beer';
+    } else {
+      return 'drink whisky';
+    }
+  };
+
+
+  const peopleWithAgeDrink = old =>
+  `drink ${old < 14 ? `toddy` : old < 18 ? `coke` : old < 21 ? `beer` : `whisky`}`;
+
+  console.log(peopleWithAgeDrink(32))
+
+  //the function starts with an arrow function syntax.
+  //inside the function there is a template literal string.
+  //there is a ternary operator (? : ) to determine the appropriate drink based on the person's age.
