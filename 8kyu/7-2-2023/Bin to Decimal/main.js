235f5e28e5b8dd9ca615a86ce8f0f8a3f2dfb70f
Jonathan Romain <romainjonathan43@gmail.com>
2023-07-02 21:56:01 -0500

    Codewars Challenge 7-2-2023

---
+++ 8kyu/7-2-2023/Bin to Decimal/main.js
@@ -0,0 +1,7 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+function binToDec(bin){
+    //the parseInt function is used to convert the binary number to decimal. The parseInt function parses a string argument and returns an integer based on the specified radix.
+    return parseInt(bin, 2)
+  }
