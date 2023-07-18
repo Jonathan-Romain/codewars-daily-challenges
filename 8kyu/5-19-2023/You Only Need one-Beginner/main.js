3f6c36f2ab2e704feee920173e69a80fde40540c
Jonathan Romain <romainjonathan43@gmail.com>
2023-07-18 17:50:12 -0500

    Codewars Challenge 7-18-2023

--- 8kyu/5-19-2023/You Only Need one-Beginner/main.js
+++ 8kyu/5-19-2023/You Only Need one-Beginner/main.js
@@ -4,12 +4,32 @@
 
 // Return true if the array contains the value, false if not.
 
-function check(a,x){
-    if(a.includes(x)){
-        return true;
-    } else{
-        return false;
-    }
-}
+// function check(a,x){
+//     if(a.includes(x)){
+//         return true;
+//     } else{
+//         return false;
+//     }
+// }
 
-const checks = (a, x) => a.includes(x)? true: false;
+// const checks = (a, x) => a.includes(x)? true: false;
+
+function expandedForm(num) {
+    let strNum = String(num); // Convert the number to a string
+    let result = [];
+    
+    for (let i = 0; i < strNum.length; i++) {
+        let digit = parseInt(strNum[i])
+    }
+    //   let digit = parseInt(strNum[i]); // Get the digit at the current position
+    //   let expandedDigit = digit * Math.pow(10, strNum.length - i - 1); // Calculate the expanded form of the digit
+      
+    //   if (expandedDigit !== 0) { // Exclude trailing zeros
+    //     result.push(expandedDigit);
+    //   }
+    // }
+    
+    // return result.join(' + '); // Join the expanded digits with " + "
+  }
+  
+  console.log(expandedForm(72));
--- 8kyu/6-10-2023/Count of positives/sum of negatives/main.js
+++ 8kyu/6-10-2023/Count of positives/sum of negatives/main.js
@@ -8,16 +8,24 @@
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 
-function countPositivesSumNegatives(input) {
-    if(input === null) return [];
-    if(input.filter(n => n != 0).length ==0 ) return [];
-    let countOfPositives = 0;
-    let sumOfNegatives = 0;
-    for(const n of input){
-      if(n < 0) sumOfNegatives += n;
-      if(n > 0) countOfPositives += 1;
-    }
-    return [countOfPositives, sumOfNegatives]
-    }
+// function countPositivesSumNegatives(input) {
+//     if(input === null) return [];
+//     if(input.filter(n => n != 0).length ==0 ) return [];
+//     let countOfPositives = 0;
+//     let sumOfNegatives = 0;
+//     for(const n of input){
+//       if(n < 0) sumOfNegatives += n;
+//       if(n > 0) countOfPositives += 1;
+//     }
+//     return [countOfPositives, sumOfNegatives]
+//     }
   
-  console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
+//   console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
+
+
+  function count(input){
+    const findPos = input.filter(value => value > 0)
+    return findPos;
+    console.log(findPos)
+  }
+  console.log(count([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
