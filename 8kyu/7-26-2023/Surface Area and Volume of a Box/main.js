be83c8219417f5769014bde7c8a0317cbb2b23e5
Jonathan Romain <romainjonathan43@gmail.com>
2023-07-26 16:37:14 -0500

    Codewars Challenge 7-26-2023

---
+++ 8kyu/7-26-2023/Surface Area and Volume of a Box/main.js
@@ -0,0 +1,11 @@
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+
+
+
+function getSize(width, height, depth){
+    let area = (2 * (width * height))+(2 * (width * depth))+(2* (height * depth));
+    let volume = width * height * depth; 
+    return [area, volume]
+  }
+  console.log(getSize(4, 2, 6))
