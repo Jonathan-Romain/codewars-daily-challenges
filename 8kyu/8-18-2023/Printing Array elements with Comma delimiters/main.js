3c34c9e79ed1d5810d52bbc3ef8b339328ce9f60
Jonathan Romain <romainjonathan43@gmail.com>
2023-08-18 13:59:43 -0500

    Codewars Challenge 8-18-2023

---
+++ 8kyu/8-18-2023/Printing Array elements with Comma delimiters/main.js
@@ -0,0 +1,17 @@
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+
+function printArray(array){
+    return array.toString()
+  }
+  console.log(printArray(["h","o","l","a"]))
+
+
+
+  const printArray = array => String(array)
