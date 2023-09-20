ac7a74a87a5311a6dcd81f57294fb8f7c896c4a0
Jonathan Romain <romainjonathan43@gmail.com>
2023-09-20 08:48:59 -0500

    Codewars Challenge 9-20-2023

---
+++ 8kyu/9-20-2023/Enumerable Magic #20 - Cascading Subsets/main.js
@@ -0,0 +1,26 @@
+// Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n, like so:
+
+// each_cons([1,2,3,4], 2)
+//   #=> [[1,2], [2,3], [3,4]]
+
+// each_cons([1,2,3,4], 3)
+//   #=> [[1,2,3],[2,3,4]]
+  
+// As you can see, the lists are cascading; ie, they overlap, but never out of order.
+
+
+
+
+function eachCons(array, n) {
+    let res = []
+    for( let i = 0; i < array.length; i++){
+        let smallarr = [];
+      for(let j = 0; j < n; j++){
+        let index = i + j;
+        if(index >= array.length) return res
+        smallarr.push(array[index])
+      }
+      res.push(smallarr)
+    }
+    return res
+  }
