769688c571bd729bae43cb7e5762482e91366003
Jonathan Romain <romainjonathan43@gmail.com>
2023-11-13 08:17:37 -0600

    Codewars Challenge 11-13-2023

--- 8kyu/11-13-2023/Color Ghost/main.js
+++ 8kyu/11-13-2023/Color Ghost/main.js
@@ -13,5 +13,5 @@
 
 
 var Ghost = function(){
-    this.color = ['white', 'yellow', 'purple', 'red'][Math.floor(Math.random * 4)]
+    this.color = ['white', 'yellow', 'purple', 'red'][Math.floor(Math.random() * 4)]
 }
