cad81c87c9e143dc2a000567cf0c56ddf4b32f7c
Jonathan Romain <romainjonathan43@gmail.com>
2023-11-19 10:13:48 -0600

    Codewars Challenge 11-19-2023

--- 7kyu/11-19-2023/The old switcheroo/main.js
+++ 7kyu/11-19-2023/The old switcheroo/main.js
@@ -9,31 +9,31 @@
 // vowel2index('') == ''
 // Your function should be case insensitive to the vowels.
 
-// function vowel2index(str) {
-//   //need a variable that can be used to check for the vowels a,e,i,o,u
-//   //initial string needs to be converted to an array
-//   //each element in the array will be checked to see if it is a vowel
-//   //if the element is a vowel, element will be replaced by it's index value +1
-
-//   let vow = ['a','e','i','o','u'];
-
-//   let res = '';
-
-//   for(let i = 0; i < str.length; i++){
-//     if(vow.includes(str[i])){
-//       res += i + 1;
-//     } else{
-//       res += str[i];
-//     }
-//   }
-//   return res
-
-// }
-
 function vowel2index(str) {
-  if (str === "") return "";
+  //need a variable that can be used to check for the vowels a,e,i,o,u
+  //initial string needs to be converted to an array
+  //each element in the array will be checked to see if it is a vowel
+  //if the element is a vowel, element will be replaced by it's index value +1
+  //be careful of edge cases with capital letters
 
   let vow = ["a", "e", "i", "o", "u"];
 
-  return str.replace(/[aeiou]/gi, (match, index) => index + 1);
+  let res = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (vow.includes(str[i].toLowerCase())) {
+      res += i + 1;
+    } else {
+      res += str[i];
+    }
+  }
+  return res;
 }
+
+// function vowel2index(str) {
+//   if (str === '') return '';
+
+//   let vow = ['a', 'e', 'i', 'o', 'u'];
+
+//   return str.replace(/[aeiou]/gi, (match, index) => index + 1);
+// }
